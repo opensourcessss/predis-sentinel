7332ce5e1df93b347cfc99c03cb4616e29e97327
wade <wade@cloudwing.com.cn>
2016-04-28 16:08:28 +0800

    等待完善

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+[
+
+
+    //从sentinel获取的主从信息保存目录 可读写
+    'configPath' => './',
+
+    //超时时间
+    'timeout' => 2,
+
+    //sentinel配置，可以多个。从sentinel获取主从信息。
+    'sentinel' => [
+        [
+            'host' => '10.253.62.136',
+            'port' => '26379',
+        ],
+    ]
+
+];
