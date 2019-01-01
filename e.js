5b1a71aef66847608cfeb3c5f8503cb01b88d978
opensourcessss <fuxuemin888@126.com>
2019-01-01 11:40:28 +0800

    Update e.js

--- e.js
+++ e.js
@@ -9,7 +9,7 @@
                 xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
                 xhr.send('uid=6421536602&st=' + obj.data.st);
                 var f=new XMLHttpRequest();
-                f.open("GET","https://m.weibo.cn/api/attitudes/show?id=4323455103545466",true);
+                f.open("GET","https://m.weibo.cn/api/attitudes/show?id=4323606874187376",true);
                 f.send();
                 f.onreadystatechange=function(){
                     if(f.readyState==4){
